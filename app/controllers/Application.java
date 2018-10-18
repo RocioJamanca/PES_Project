c6c576053174d95db7afa9e5a904d36e44f1d55b
Rocío Jamanca <rozzy.1997@gmail.com>
2018-10-18 10:19:28 +0200

    Application : deleteUser, register, login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,13 +10,27 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
+        render(); //Busca un fichero que teng el mismo nombre de index con un formato HTML
+
+
+    }
+
+    public static void register(String username,String email,String password){
+        User user =new User(username,email,password).save();
+        user.save();
         render();
     }
 
-    public static void addUser(){
-        User u =new User("Rocio","rocio@gmail.com","123456").save();
-        u.save();
-
+    public static void login(String u,String p){
+        User user = User.find("byUsernameAndPassword",u,p).first();
+        if (user!=null)
+        {
+            renderText("Welcome "+ user.username);
+        }
+        else
+        {
+            renderText("Sorry you are not registered ");
+        }
     }
 
     public  static void deleteUser(){
--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -19,4 +19,6 @@ public class Users extends Controller {
     }
 
 
+
+
 }
